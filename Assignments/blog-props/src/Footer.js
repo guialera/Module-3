4601aae33a4a8a5f9c4bf62edc6ad243a2cf055f
Alex Ramirez <guialera@gmail.com>
2020-11-27 12:42:26 -0800

    Committing an updated partially completed Blog Props assignment V2 11/27/2020

--- Assignments/blog-props/src/Footer.js
+++ Assignments/blog-props/src/Footer.js
@@ -4,6 +4,12 @@ function Footer(){
     return(
         <div>
             <hr></hr>
+            <div className="footer">
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-github"></i>
+                <p className="copy">Copyright © Your Website 2020</p>
+            </div>
         </div>
     )
 }
