ec29179577469ff8d44bc76d2ad6b75d253825db
Alex Ramirez <guialera@gmail.com>
2020-12-12 19:59:31 -0800

    Committing a completed Name Badge assignment 12/12/2020

--- Assignments/name-badge/src/App.js
+++ Assignments/name-badge/src/App.js
@@ -117,69 +117,91 @@ class App extends React.Component {
         })
         return (
             <div>
+            <div className="inputDiv">
+                <h1 className="header">Hello My Name Is</h1>
                 <form onSubmit={this.submitInfo}>
-                    <input
+                    <input className="firstName"
                         type="text"
                         name="firstName"
                         placeholder="First Name"
                         value={this.state.firstName}
                         onChange={this.fillIn}
+                        minLength="3"
+                        required
                     />
 
-                    <input
+                    <input className="lastName"
                         type="text"
                         name="lastName"
                         placeholder="Last Name"
                         value={this.state.lastName}
                         onChange={this.fillIn}
+                        minLength="3"
+                        required
                     />
 
-                    <input
+                    <input className="email"
                         type="email"
                         name="email"
                         placeholder="Email"
                         value={this.state.email}
                         onChange={this.fillIn}
+                        minLength="3"
+                        required
                     />
 
-                    <input
+                    <input className="placeOfBirth"
                         type="text"
                         name="placeOfBirth"
                         placeholder="Place of Birth"
                         value={this.state.placeOfBirth}
                         onChange={this.fillIn}
+                        minLength="3"
+                        required
                     />
 
-                    <input
-                        type="number"
+                    <input className="phone"
+                        type="tel"
                         name="phone"
                         placeholder="Phone Number"
                         value={this.state.phone}
                         onChange={this.fillIn}
+                        pattern="[0-9]+"
+                        minLength="3"
+                        required
                     />
 
-                    <input
+                    <input className="favFood"
                         type="text"
                         name="favFood"
                         placeholder="Favorite Food"
                         value={this.state.favFood}
                         onChange={this.fillIn}
+                        minLength="3"
+                        required
                     />
 
-                    <textarea
+                    <textarea className="About"
+                        rows="8"
+                        cols="10"
                         type="text"
                         name="About"
                         placeholder="About Yourself"
                         value={this.state.About}
                         onChange={this.fillIn}
+                        minLength="3"
+                        required
                     />
 
-                    <input
+                    <input className="submitButton"
                         type="submit"
                         value="Submit Information"
                     />
                 </form>
-                {people}
+                </div>
+                <div>
+                    {people}
+                </div>
             </div>
         )
     }
--- Assignments/name-badge/src/Badge.js
+++ Assignments/name-badge/src/Badge.js
@@ -1,15 +1,16 @@
 import React from "react"
 
-function Badge(props){
-    return(
-        <div>
-            <p>First Name: {props.data.firstName}</p>
-            <p>Last Name: {props.data.lastName}</p>
-            <p>Email: {props.data.email}</p>
-            <p>Place of Birth: {props.data.placeOfBirth}</p>
-            <p>Phone Number: {props.data.phone}</p>
-            <p>Favorite Food: {props.data.favFood}</p>
-            <p>About Your Self: {props.data.About}</p>
+function Badge(props) {
+    return (
+        <div className="badgeDiv">
+            <h1 className="badgeName">Hello My Name Is</h1>
+            <p className="badgeFirstName">First Name: {props.data.firstName}</p>
+            <p className="badgeLastName">Last Name: {props.data.lastName}</p>
+            <p className="badgeEmail">Email: {props.data.email}</p>
+            <p className="badgePlaceOfBirth">Place of Birth: {props.data.placeOfBirth}</p>
+            <p className="badgePhone">Phone Number: {props.data.phone}</p>
+            <p className="badgeFavFood">Favorite Food: {props.data.favFood}</p>
+            <p className="badgeAbout">About Your Self: {props.data.About}</p>
         </div>
     )
 }
